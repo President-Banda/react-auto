851c460e16659472a0b589d5aa9ee7f9c2cdbb33
curhvyn <michaelcurhvyn@gmail.com>
2022-03-15 19:52:00 +0000

    Added Script to install nodejs. Set up the frontend project.

--- install.sh
+++ install.sh
@@ -12,16 +12,10 @@ echo "Environment Updated Successfully"
 sudo apt-get install curl -y
 echo "Successfully installed \'curl\'"
 
-###### INSTALL NVM [Node Vesion Manager] 
-# Or Install Node source
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-nvm install 14.4.0
-echo "Successfully Installed NVM and Nodejs"
-
-# Or Use Node Source
-# curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-# echo "Successfully Installed Nodejs"
+#install nodejs ubuntu
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
 
 ###### INSTALL AND START NGINX
 sudo apt-get install nginx -y
@@ -30,20 +24,14 @@ echo "Nginx Installed Successfully!"
 sudo systemctl start nginx
 echo "Nginx server started successfully!"
 
-###### CHANGE HTML DIRECTORY PERMISSIONS
-chown -R ubuntu /var/www/html
-chown -R 755 /var/www/html
-echo "Permissions Set Successfully"
+#clone the frontend react app
+cd /var/www/html
+sudo git clone https://github.com/President-Banda/Mobalysis.git
+cd Mobalysis/frontend
+sudo npm install
+sudo npm run build
 
-###### CHANGE INTO /VAR/WWW/HTML
-cd /var/ww/html/
-echo "Changed Into /var/ww/html"
-
-###### CREATE THE REACT APP
-npx create-react-app react-sample
-echo "Created New/Sample React App Successfully!"
-
-###### START THE APP
-cd react-sample
-npm start
-echo "App Started Successfully on Port 3000!"
+#Copy the frontend build to the nginx server
+sudo cp -r /var/www/html/Mobalysis/frontend/build/* /var/www/html/
+sudo -rf index.nginx.debian.html
+echo "App Started Successfully!"
